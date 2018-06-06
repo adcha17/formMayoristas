7a54e2ced2c71fb3fb86b00c493914b38f76bc91
adcha <adcha17@gmail.com>
2018-06-06 00:26:47 -0500

    structure angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {
+  MatToolbarModule,
+  MatTabsModule,
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+  MatCheckboxModule
+} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +20,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
